cb4c69d6f088237358af56b1def940505a91391d
Ryel Banfield <ryelbanfield@gmail.com>
2021-05-19 10:32:00 -0400

    Add navbar, header and image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import './style.css';
+import './style.css'
+import HeaderImage from './images/background.jpg'
+
+const content = document.querySelector('#content')
+
+// navbar navbar-dark bg-dark d-flex justify-content-center"
+const navbar = document.createElement('nav')
+navbar.classList.add("navbar", "navbar-dark", "bg-dark", "d-flex", "justify-content-center")
+navbar.innerHTML = `
+<ul class="navbar-nav flex-row">
+  <li class="nav-item mx-3"> <a class='home nav-link hvr-pulse' href="#">Home</a> </li>
+  <li class="nav-item mx-3"> <a class='menu nav-link hvr-pulse' href="#">Menu</a> </li>
+  <li class="nav-item mx-3"> <a class='contact nav-link hvr-pulse' href="#">Contact</a> </li>
+</ul>`
+content.appendChild(navbar)
+
+const header = document.createElement('header')
+content.appendChild(header)
+
+const headerImage = new Image()
+headerImage.src = HeaderImage
+headerImage.classList.add('header-image')
+header.appendChild(headerImage)
+
+const headerTextDiv = document.createElement('div')
+headerTextDiv.classList.add('header-text-div')
+header.appendChild(headerTextDiv)
+
+const headerText = document.createElement('h1')
+headerText.classList.add('header-text')
+headerText.innerHTML = 'CaszNights'
+headerTextDiv.appendChild(headerText)
